b486a0eaf85c34891d1624d7d9449138568c3b24
Simon Darby <simon@recreational.studio>
2026-05-14 15:31:18 +1200

    Update Lot 168 overrides from Airtable

---
+++ lots/lot-168.js
@@ -0,0 +1,14 @@
+window.ROSSBURN_LOT_OVERRIDES=window.ROSSBURN_LOT_OVERRIDES||{};
+window.ROSSBURN_LOT_OVERRIDES["168"]={
+  "n":"Lot 168",
+  "p":"Brookside",
+  "s":"3",
+  "st":"For Sale",
+  "pr":"260000",
+  "t":"Homesite",
+  "sz":"371",
+  "d":"",
+  "i1":"",
+  "i2":"",
+  "i3":""
+};
