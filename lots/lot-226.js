cf4c5db9c5bbc3528d1a7c2139e19daaf7a0a47e
Simon Darby <simon@recreational.studio>
2026-05-14 15:24:48 +1200

    Create lot-226.js

---
+++ lots/lot-226.js
@@ -0,0 +1,2 @@
+window.ROSSBURN_LOT_OVERRIDES=window.ROSSBURN_LOT_OVERRIDES||{};
+window.ROSSBURN_LOT_OVERRIDES["226"]={"n":"Lot 226","p":"Brookside","s":"1","st":"For Sale","pr":"365000","t":"Homesite","sz":"519","d":"Brookside homesites enjoy easy access to the reserve.","i1":"","i2":"","i3":""};
