6ccf2730ff96515a27173568a404f6a25e2f2eff
Simon Darby <simon@recreational.studio>
2026-05-16 15:28:27 +1200

    Update Lot 45 overrides from Airtable

---
+++ lots/lot-45.js
@@ -0,0 +1,14 @@
+window.ROSSBURN_LOT_OVERRIDES=window.ROSSBURN_LOT_OVERRIDES||{};
+window.ROSSBURN_LOT_OVERRIDES["45"]={
+  "n":"Lot 45",
+  "p":"Market",
+  "s":"4",
+  "st":"Not Available",
+  "pr":"0",
+  "t":"Homesite",
+  "sz":"504",
+  "d":"Rossburn homesite description",
+  "i1":"",
+  "i2":"",
+  "i3":""
+};
