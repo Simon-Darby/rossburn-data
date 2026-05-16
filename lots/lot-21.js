56b8261f4d3f8d9149a8441924f473bbf148343e
Simon Darby <simon@recreational.studio>
2026-05-16 15:28:24 +1200

    Update Lot 21 overrides from Airtable

---
+++ lots/lot-21.js
@@ -0,0 +1,14 @@
+window.ROSSBURN_LOT_OVERRIDES=window.ROSSBURN_LOT_OVERRIDES||{};
+window.ROSSBURN_LOT_OVERRIDES["21"]={
+  "n":"Lot 21",
+  "p":"Brookside",
+  "s":"1",
+  "st":"For Sale",
+  "pr":"0",
+  "t":"Homesite",
+  "sz":"609",
+  "d":"Rossburn homesite description",
+  "i1":"",
+  "i2":"",
+  "i3":""
+};
